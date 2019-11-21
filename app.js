eaf3fcd22c668a16ab2dd760944d14b40d0ae0df
eshan <eshan786behal@gmail.com>
2019-11-21 10:42:15 +0530

    created server.

---
+++ app.js
@@ -0,0 +1,17 @@
+//creating this project with pure node.
+
+//require node modules
+const http = require('http');
+
+//file imports
+
+//connection settings
+const port = process.env.port || 3000;
+
+//create server
+const server = http.createServer(respond);
+
+//for listening to the client requests on specific port, the port is to be available.
+server.listen(port, () =>{
+    console.log(`listening on port: ${port}`);
+});
